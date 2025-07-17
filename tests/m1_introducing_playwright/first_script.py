0d10169fded1e6674a556a0e5c4d7285f65ae49f
ron_kaiser <ron.kaiser@shopic.co>
2025-07-17 17:27:16 +0300

    First script to open and close browser

---
+++ tests/m1_introducing_playwright/first_script.py
@@ -0,0 +1,7 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+    page = browser.new_page()
+    page.goto("https://playwright.dev/python/")
+    browser.close()
