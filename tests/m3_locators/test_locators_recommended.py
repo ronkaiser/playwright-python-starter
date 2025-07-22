603da00df74a316f9050560b4a9c53cd6b317ca2
ron_kaiser <ron.kaiser@shopic.co>
2025-07-22 12:56:21 +0300

    Test locators

--- tests/m3_locators/test_locators_recommended.py
+++ tests/m3_locators/test_locators_recommended.py
@@ -9,3 +9,15 @@ def test_headless_and_slow_mo(browser_type: BrowserType):
 
 def test_recommended_locators(page: Page):
     page.goto(BASE_URL)
+
+    first_name = page.get_by_label('First name')
+    
+    first_name.fill('Sofia')
+    first_name.clear()
+
+    page.get_by_label('First name').fill('Ron')
+
+    page.get_by_role('button', name='Register', exact=True).click()
+
+    warning = page.get_by_text('Valid last name is required')
+    expect(warning).to_be_visible()
